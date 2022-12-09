61fbe14ba243ea4b7c3db0b605c1ea967fd11590
tony555t <tonytullah764@gmail.com>
2022-12-09 11:46:52 +0300

    done

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-function isPalindrome(word) {
+//function isPalindrome(word) {
   // Write your algorithm here
-}
+  function isPalindrome(string) {
+    let left = 0;
+    let right = string.length - 1;
+  
+    while (left <= right) {
+      if (string[left] !== string[right]) return false;
+      left++;
+      right--;
+    }
+  
+  return true;
+  }
+//}
 
 /* 
   Add your pseudocode here
 */
+// function ispalindrome(){
+//return  true
+//}
 
 /*
   Add written explanation of your solution here
 */
+// we added two pointer to give the dirrection to where the name wiil be read from
+//we us the while to identify if the  condition is met
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
